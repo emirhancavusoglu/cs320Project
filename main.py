9803664b009f03c9170be7f50aeb4c27e2cdce37
emirhancavusoglu <emirhan.cavusoglu.20513@ozu.edu.tr>
2022-12-23 17:08:23 +0300

    committ

--- main.py
+++ main.py
@@ -7,25 +7,28 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 
 url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
-parameters = { 'slug': 'binance-coin', 'convert': 'USD'}
-
+bitcoin = { 'slug': 'bitcoin', 'convert': 'USD'}
+binancecoin = { 'slug': 'binance-coin', 'convert': 'USD'}
 headers = {
   'Accepts': 'application/json',
   'X-CMC_PRO_API_KEY': '2d444a4e-7303-4551-8276-df65bb537278',
 }
+coins = [bitcoin,binancecoin]
 
 session = Session()
 session.headers.update(headers)
 
 try:
-  response = session.get(url, params = parameters)
+  response = session.get(url, params = bitcoin)
+  response2 = session.get(url, params = binancecoin)
   data = json.loads(response.text)['data']['1']['quote']['USD']['price']
-  print(data)
+  data2 = json.loads(response2.text)['data']['1839']['quote']['USD']['price']
 
-
-
-
-
+  for i in coins :
+    if (i==bitcoin):
+      print(data)
+    if (i==binancecoin):
+      print(data2)
 
 except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
