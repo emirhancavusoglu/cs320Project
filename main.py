896cb680f93c9dddc3ab69e84f83c8a5dbd3f1fe
emirhancavusoglu <emirhan.cavusoglu.20513@ozu.edu.tr>
2022-12-23 18:19:48 +0300

    entering the datas which are Coins & Shares

--- main.py
+++ main.py
@@ -7,29 +7,49 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 
 url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
-bitcoin = { 'slug': 'bitcoin', 'convert': 'USD'}
-binancecoin = { 'slug': 'binance-coin', 'convert': 'USD'}
 headers = {
   'Accepts': 'application/json',
   'X-CMC_PRO_API_KEY': '2d444a4e-7303-4551-8276-df65bb537278',
 }
-coins = [bitcoin,binancecoin]
+bitcoin = { 'symbol': 'BTC', 'convert': 'USD'}
+ethereum = { 'symbol': 'ETH', 'convert': 'USD'}
+tether = { 'symbol': 'USDT', 'convert': 'USD'}
+bnb = { 'symbol': 'BNB', 'convert': 'USD'}
+xrp = { 'symbol': 'XRP', 'convert': 'USD'}
+dogecoin = { 'symbol': 'DOGE', 'convert': 'USD'}
+cardano = { 'symbol': 'ADA', 'convert': 'USD'}
+litecoin = { 'symbol': 'LTC', 'convert': 'USD'}
+polygon = { 'symbol': 'MATIC', 'convert': 'USD'}
+solana = { 'symbol': 'SOL', 'convert': 'USD'}
+shiba = { 'symbol': 'SHIB', 'convert': 'USD'}
+dai = { 'symbol': 'DAI', 'convert': 'USD'}
+polkadot = { 'symbol': 'DOT', 'convert': 'USD'}
+tron = { 'symbol': 'TRX', 'convert': 'USD'}
+uniswap = { 'symbol': 'UNI', 'convert': 'USD'}
+
+coins = []
 
 session = Session()
 session.headers.update(headers)
 
 try:
   response = session.get(url, params = bitcoin)
-  response2 = session.get(url, params = binancecoin)
+
+
   data = json.loads(response.text)['data']['1']['quote']['USD']['price']
-  data2 = json.loads(response2.text)['data']['1839']['quote']['USD']['price']
+  #data2 = json.loads(response2.text)['data']['1839']['quote']['USD']['price']
+
+  print("Enter the value that you want to enter share")
+  miktar = int(input())
+
+  for ix in range(miktar):
+    share = int(input())
+    coins.insert(ix, share)
 
   for i in coins :
-    if (i==bitcoin):
+    if (i==1):
       print(data)
-    if (i==binancecoin):
-      print(data2)
+
 
 except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
-
